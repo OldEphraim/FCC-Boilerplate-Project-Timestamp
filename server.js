e0b32d2ec318041680eefbc70614603d4a157fcb
oldephraim <a8garber@yahoo.com>
2022-02-18 01:40:14 -0600

    timestamp project

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,12 +19,25 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api", function(req, res) {
+  res.json({unix: new Date().getTime(), "utc": new Date().toUTCString()})
+})
 
+app.get("/api/:date?", function(req, res, err) {
+  let dateString = req.params.date.toString();
+  if ((parseInt(dateString) !== NaN) && (dateString.search(/-/g) == -1) && (dateString.search(/\s/g) == -1)) {
+    res.json({"unix": new Date(parseInt(dateString)).getTime(), "utc": new Date(parseInt(dateString)).toUTCString()});
+  } else if (new Date(dateString) == "Invalid Date") {
+    res.json({"error": "Invalid Date"});
+  } else {
+    res.json({"unix": new Date(dateString).getTime(), "utc": new Date(dateString).toUTCString()});
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
